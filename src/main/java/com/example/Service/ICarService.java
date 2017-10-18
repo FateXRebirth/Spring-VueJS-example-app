c759241a1df9b7bd3b120da8de478e7b26c41d1f
FateXRebirth <FateXRebirth@gmail.com>
2017-10-18 11:09:41 +0800

    Spring Boot Mustache tutorial

---
+++ src/main/java/com/example/Service/ICarService.java
@@ -0,0 +1,13 @@
+package com.example.Service;
+
+import com.example.Entity.Car;
+import java.util.List;
+
+/**
+ * Created by FateXRebirth on 18/10/2017.
+ */
+
+public interface ICarService {
+
+    public List<Car> findAll();
+}
