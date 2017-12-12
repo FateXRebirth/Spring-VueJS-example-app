8c744da521c744dbd12ba46053036a76cf3118d0
Fate:Rebirth <FateXRebirth@gmail.com>
2017-12-12 12:24:24 +0800

    Refactor application structure

---
+++ backend/src/main/java/example/Controller/APIController.java
@@ -0,0 +1,7 @@
+package example.Controller;
+
+/**
+ * Created by FateXRebirth on 20/10/2017.
+ */
+public class APIController {
+}
--- src/main/java/example/Controller/APIController.java
+++
@@ -1,7 +0,0 @@
-package example.Controller;
-
-/**
- * Created by FateXRebirth on 20/10/2017.
- */
-public class APIController {
-}
