a3bb2fa066f27f3cfb51291cf1db5b5cb74c61c0
Kevin Peng <FateXRebirth@gmail.com>
2018-10-01 23:31:02 +0800

    Backend - Dashboard design

--- backend/src/main/resources/static/js/script.js
+++ backend/src/main/resources/static/js/script.js
@@ -24,3 +24,198 @@ document.addEventListener('DOMContentLoaded', function () {
   }
 
 });
+
+// Regular Expression
+var Pattern = {
+    email: /^[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}$/,
+}
+
+// Create custom message
+var message = function(message) {
+    var element = document.createElement("p");
+    element.className = "help is-danger ";
+    element.innerText = message;
+    return element;
+}
+
+// Check input dynamically
+var check = function(event, type) {
+
+    var element = event.target;
+    var error = $(element).parent().next();
+    error.empty();
+
+    switch(type) {
+
+        case "username":
+            if(element.value.length == 0 ) {
+                error.append(message("Username Required"));
+            } else if (element.value.length < 4 || element.value.length > 20 ) {
+                error.append(message("Length of username should between 4 and 20"));
+            } else {
+                error.empty()
+            }
+        break;
+
+        case "password":
+            if(element.value.length == 0 ) {
+                error.append(message("Password Required"));
+            } else if (element.value.length < 4 || element.value.length > 20 ) {
+                error.append(message("Length of password should between 4 and 20"));
+            } else {
+                error.empty()
+            }
+        break;
+
+        case "email":
+            if(element.value.length == 0 ) {
+              error.append(message("Email Required"));
+            } else if (!Pattern.email.test(element.value)) {
+              error.append(message("Email Invalid"));
+            } else {
+                error.empty()
+            }
+        break;
+
+        case "confirmation":
+            if(element.value.length == 0 ) {
+                error.append(message("Confirmation Required"));
+            } else if (element.value.length < 4 || element.value.length > 20 ) {
+                error.append(message("Length of confirmation should between 4 and 20"));
+            } else {
+                error.empty()
+            }
+        ;
+        break;
+    }
+
+}
+
+// Validate login form
+$('#loginForm').submit(function(event) {
+
+    var exist = false;
+
+    $('.message-body').empty();
+
+    var username = $('input[name=username]');
+    var password = $('input[name=password]');
+
+    if($('.help.is-danger').length > 0) return false;
+
+    if(username.val() == "" || password.val() == "") {
+        if(username.val() == "") username.parent().next().append(message("Username Required"));
+        if(password.val() == "") password.parent().next().append(message("Password Required"));
+        return false;
+    } else {
+        $.ajax("http://localhost:8081/api/get/person/all", { async: false })
+        .done(function(data) {
+            data.forEach( function(person) {
+                if(person.username == username.val()) {
+                    exist = true;
+                    if(person.password == password.val()) {
+                        $('#success-message .message-body').append(message("Successfully!"));
+                        $('#success-message').fadeIn();
+                        setTimeout(function() {
+                           $('#success-message').fadeOut();
+                        }, 1500)
+                        return false;
+                    } else {
+                        $('#failure-message .message-body').append(message("Password is not correct"));
+                    }
+                }
+            })
+            if(!exist) {
+                $('#failure-message .message-body').append(message("Username does not exist"));
+            }
+        })
+    }
+
+    if($('#failure-message .message-body').children().length > 0) {
+        $('#failure-message').fadeIn();
+        setTimeout(function() {
+            $('#failure-message').fadeOut();
+        }, 1500)
+        return false;
+    }
+})
+
+// Validate register form
+$('#registerForm').submit(function(event) {
+
+    event.preventDefault();
+
+    var exist = false;
+
+    $('.message-body').empty();
+
+    var username = $('input[name=username]');
+    var email = $('input[name=email]');
+    var password = $('input[name=password]');
+    var confirmation = $('input[name=confirmation]');
+
+    if($('.help.is-danger').length > 0) return false;
+
+    if(username.val() == "" || password.val() == "") {
+        if(username.val() == "") username.parent().next().append(message("Username Required"));
+        if(email.val() == "") email.parent().next().append(message("E-mail Required"));
+        if(password.val() == "") password.parent().next().append(message("Password Required"));
+        if(confirmation.val() == "") confirmation.parent().next().append(message("Confirmation Required"));
+        return false;
+    } else {
+        $.ajax("http://localhost:8081/api/get/person/email", { async: false })
+        .done(function(data) {
+            data.forEach( function(Email) {
+                if(Email == email.val()) {
+                    exist = true;
+                    $('#failure-message .message-body').append(message("This email has been used"));
+                }
+            })
+        })
+        if(!exist) {
+            if(password.val() == confirmation.val()) {
+                var data = {
+                    username: username.val(),
+                    email: email.val(),
+                    password: password.val(),
+                    type: 'user'
+                }
+                $.ajax({
+                    async: false,
+                    type: "POST",
+                    contentType: "application/json",
+                    url: "http://localhost:8081/api/post/person",
+                    data: JSON.stringify(data),
+                    dataType: 'json',
+                    success: function (data) {
+                        console.log("success")
+                    },
+                    error: function (e) {
+                        console.log("failure")
+                    }
+                });
+                $('#success-message .message-body').append(message("Successfully! You can login now"));
+                $('#success-message').fadeIn();
+                setTimeout(function() {
+                   $('#success-message').fadeOut();
+                   window.location.replace("/login")
+                }, 1500)
+                return false;
+            } else {
+                $('#failure-message .message-body').append(message("Password should be same"));
+            }
+        }
+    }
+
+    if($('#failure-message .message-body').children().length > 0) {
+        $('#failure-message').fadeIn();
+        setTimeout(function() {
+            $('#failure-message').fadeOut();
+        }, 1500)
+        return false;
+    }
+})
+
+$(function() {
+    console.log("document ready")
+})
