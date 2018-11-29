002aa60998e1492fb9f877f76ed758c0b2d59a22
Kevin Peng <FateXRebirth@gmail.com>
2018-11-29 22:40:26 +0800

    Frontend - Useful component

---
+++ frontend/pages/sample.vue
@@ -0,0 +1,21 @@
+<template>
+  <div>
+    <carousel/>
+    <modal/>
+    <nav-menu/>
+  </div>
+</template>
+
+<script>
+import Carousel from '~/components/Carousel.vue';
+import Modal from '~/components/Modal.vue';
+import NavMenu from '~/components/NavMenu.vue';
+
+export default {
+  components: {
+    Carousel,
+    Modal,
+    NavMenu
+  },
+}
+</script>
