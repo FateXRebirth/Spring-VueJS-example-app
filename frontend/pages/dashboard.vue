d161729cc4f4ee83d4125468398f5c226cd24b23
Kevin Peng <FateXRebirth@gmail.com>
2018-12-03 23:40:38 +0800

    Frontend - Design Dashboard

---
+++ frontend/pages/dashboard.vue
@@ -0,0 +1,48 @@
+<template>
+  <main class="dashboard">
+    <div class="container">
+       <section class="dashboard__header">
+        <logo/><h1>NUXT</h1>
+        <h3>Universal Vue.js Applications</h3>
+      </section>
+      <hr class="hr-30">
+      <nav-menu/>
+    </div>
+   
+  </main>
+</template>
+
+<script>
+import Logo from '~/components/Logo.vue';
+import Carousel from '~/components/Carousel.vue';
+import NavMenu from '~/components/NavMenu.vue';
+
+export default {
+  components: {
+    Logo,
+    Carousel,
+    NavMenu
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+.dashboard {
+  height: 2000px;
+  background: url('https://source.unsplash.com/1920x1080/?car');
+  &__header {
+    text-align: center;
+    color: #35495e;
+    h1 {
+      display: inline-block;
+      margin-left: 20px;
+      font-size: 80px;
+      font-weight: 400;
+    }
+    h3 {
+      font-size: 40px;
+      font-weight: 200;
+    }
+  }
+}
+</style>
--- frontend/pages/index.vue
+++ frontend/pages/index.vue
@@ -22,6 +22,11 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo,
+  },
+  mounted(redirect) {
+    setTimeout(function() {
+      window.location.href = 'dashboard';
+    }, 3000)
   }
 }
 </script>
