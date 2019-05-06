da1dbed8ca92d081d8fb4f212bb11f11b296030d
Yu-Min Peng <FateXRebirth@gmail.com>
2019-05-06 08:10:40 +0800

    Frontend - Car Create Cont.

--- frontend/pages/dashboard/carNew.vue
+++ frontend/pages/dashboard/carNew.vue
@@ -14,71 +14,71 @@
         <el-row :gutter="10">
           <el-col :span="5">
             <el-form-item label="Brand" prop="brand">
-              <Select :data="BrandOptions" type="Brand" @callback="GetValue" />
+              <Select :data="BrandOptions" type="Brand" @callback="Callback" />
             </el-form-item>
             </el-col>
           <el-col :span="6">
             <el-form-item label="Series" prop="series">
-              <Select :data="SeriesOptions" type="Series" @callback="GetValue" />
+              <Select :data="SeriesOptions" type="Series" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="5">
             <el-form-item label="Category" prop="category">
-              <Select :data="CategoryOptions" type="Category" @callback="GetValue" />
+              <Select :data="CategoryOptions" type="Category" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="Year" prop="year">
-              <Select :data="YearOptions" type="Year" @callback="GetValue" />
+              <Select :data="YearOptions" type="Year" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="Month" prop="month">
-              <Select :data="MonthOptions" type="Month" @callback="GetValue" />
+              <Select :data="MonthOptions" type="Month" @callback="Callback" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row :gutter="10">
           <el-col :span="4">
             <el-form-item label="Transmission" prop="transmission">
-              <Select :data="TransmissionOptions" type="Transmission" @callback="GetValue" />
+              <Select :data="TransmissionOptions" type="Transmission" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="GearType" prop="gearType">
-              <Select :data="GearTypeOptions" type="GearType" @callback="GetValue" />
+              <Select :data="GearTypeOptions" type="GearType" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="GasType" prop="gasType">
-              <Select :data="GasTypeOptions" type="GasType" @callback="GetValue" />
+              <Select :data="GasTypeOptions" type="GasType" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
-            <el-form-item label="Engine" prop="engine">
-              <Select :data="EngineOptions" type="Engine" @callback="GetValue" />
+            <el-form-item label="EngineDisplacement" prop="engineDisplacement">
+              <Select :data="EngineDisplacementOptions" type="EngineDisplacement" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="Passenger" prop="passenger">
-              <Select :data="PassengerOptions" type="Passenger" @callback="GetValue" />
+              <Select :data="PassengerOptions" type="Passenger" @callback="Callback" />
             </el-form-item>
           </el-col>
           <el-col :span="4">
             <el-form-item label="Color" prop="color">
-              <Select :data="ColorOptions" type="Color" @callback="GetValue" />
+              <Select :data="ColorOptions" type="Color" @callback="Callback" />
             </el-form-item>
           </el-col>
         </el-row>
         <el-row :gutter="10">
           <el-col :span="6">
             <el-form-item label="Mileage" prop="mileage">
-              <el-input v-model="carForm.mileage" placeholder="mileage here"><template slot="append">公里</template></el-input>
+              <el-input v-model="carForm.mileage" placeholder="Mileage"><template slot="append">公里</template></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="6">
             <el-form-item label="Price" prop="price">
-              <el-input v-model="carForm.price" placeholder="price here"><template slot="append">萬</template></el-input>
+              <el-input v-model="carForm.price" placeholder="Price"><template slot="append">萬</template></el-input>
             </el-form-item>
           </el-col>
           <el-col :span="6"></el-col>
@@ -87,16 +87,16 @@
     
         <Header title="Equipment" />
         <hr class="hr-30">
-        <el-checkbox :indeterminate="EquipmentIsIndeterminate1" v-model="EquipmentCheckAll1" @change="handleCheckAllChange1"> 內外裝配備 </el-checkbox>
+        <el-checkbox :indeterminate="EquipmentIsIndeterminate" v-model="EquipmentCheckAll" @change="EquipmentAllChange"> 內外裝配備 </el-checkbox>
         <div style="margin: 15px 0;"></div>
-        <el-checkbox-group v-model="EquipmentCheckedOption1" @change="handleCheckedCitiesChange1">
-          <el-checkbox-button v-for="option in EquipmentOption1" :label="option" :key="option">{{option}}</el-checkbox-button>
+        <el-checkbox-group v-model="EquipmentCheckedOption" @change="CheckedEquipmentChange">
+          <el-checkbox-button v-for="option in EquipmentOptions" :label="option" :key="option.value">{{option.label}}</el-checkbox-button>
         </el-checkbox-group>
         <hr class="hr-30">
-        <el-checkbox :indeterminate="EquipmentIsIndeterminate2" v-model="EquipmentCheckAll2" @change="handleCheckAllChange2"> 安全配備 </el-checkbox>
+        <el-checkbox :indeterminate="SafetyIsIndeterminate" v-model="SafetyCheckAll" @change="SafetyAllChange"> 安全配備 </el-checkbox>
         <div style="margin: 15px 0;"></div>
-        <el-checkbox-group v-model="EquipmentCheckedOption2" @change="handleCheckedCitiesChange2">
-          <el-checkbox-button v-for="option in EquipmentOption2" :label="option" :key="option">{{option}}</el-checkbox-button>
+        <el-checkbox-group v-model="SafetyCheckedOption" @change="CheckedSafetyChange">
+          <el-checkbox-button v-for="option in SafetyOptions" :label="option" :key="option.value">{{option.label}}</el-checkbox-button>
         </el-checkbox-group>
         <hr class="hr-30">
         <Header title="Photos" />
@@ -125,17 +125,17 @@
         <hr class="hr-30">
         <div class="contactInfo">
           <el-form-item prop="name">
-            <el-input v-model="carForm.name" placeholder="name here">
+            <el-input v-model="carForm.name" placeholder="Name">
               <template slot="prepend"><i class="el-icon-info"></i>Name</template>
             </el-input>
           </el-form-item>
           <el-form-item prop="cellphone">
-           <el-input v-model="carForm.cellphone" placeholder="cellphone here">
+           <el-input v-model="carForm.cellphone" placeholder="Cellphone">
             <template slot="prepend"><i class="el-icon-phone"></i>Cellphone</template>
           </el-input>
           </el-form-item>
           <el-form-item prop="address">
-           <el-input v-model="carForm.address" placeholder="address here">
+           <el-input v-model="carForm.address" placeholder="Address">
             <template slot="prepend"><i class="el-icon-location"></i>Address</template>
           </el-input>
           </el-form-item>
@@ -154,9 +154,6 @@ import Breadcrumb from '~/components/Breadcrumb.vue';
 import Header from '~/components/Header.vue';
 import Select from '~/components/Select.vue';
 
-const EquipmentOption1 = ['衛星導航', '恆溫空調', '影音系統', '免鑰匙啟閉系統', '電動座椅', '皮椅', '方向盤控制鈕', 'HID', '電動滑側門', '天窗'];
-const EquipmentOption2 = ['日行燈', 'ABS', '巡跡防滑系統', '胎壓偵測', '倒車影像', '定速', '車側盲點偵測系統', '車道偏移系統', '安全氣囊'];
-
 export default {
   components: {
     SideMenu,
@@ -171,24 +168,28 @@ export default {
     // let CategoryOptions = await app.$axios.get('http://127.0.0.1:8080/CategoryOptions.json');
     let YearOptions = await app.$axios.get('http://127.0.0.1:8080/YearOptions.json');
     let MonthOptions = await app.$axios.get('http://127.0.0.1:8080/MonthOptions.json');
-    // let TransmissionOptions = await app.$axios.get('http://127.0.0.1:8080/TransmissionOptions.json');
-    // let GearTypeOptions = await app.$axios.get('http://127.0.0.1:8080/GearTypeOptions.json');
-    // let GasTypeOptions = await app.$axios.get('http://127.0.0.1:8080/GasTypeOptions.json');
-    // let EngineOptions = await app.$axios.get('http://127.0.0.1:8080/EngineOptions.json');
-    // let PassengerOptions = await app.$axios.get('http://127.0.0.1:8080/PassengerOptions.json');
-    // let ColorOptions = await app.$axios.get('http://127.0.0.1:8080/ColorOptions.json');
+    let TransmissionOptions = await app.$axios.get('http://127.0.0.1:8080/TransmissionOptions.json');
+    let GearTypeOptions = await app.$axios.get('http://127.0.0.1:8080/GearTypeOptions.json');
+    let GasTypeOptions = await app.$axios.get('http://127.0.0.1:8080/GasTypeOptions.json');
+    let EngineDisplacementOptions = await app.$axios.get('http://127.0.0.1:8080/EngineDisplacementOptions.json');
+    let PassengerOptions = await app.$axios.get('http://127.0.0.1:8080/PassengerOptions.json');
+    let ColorOptions = await app.$axios.get('http://127.0.0.1:8080/ColorOptions.json');
+    let EquipmentOptions = await app.$axios.get('http://127.0.0.1:8080/EquipmentOptions.json');
+    let SafetyOptions = await app.$axios.get('http://127.0.0.1:8080/SafetyOptions.json');
     return {
       // BrandOptions: BrandOptions.data,
       // SeriesOptions: SeriesOptions.data,
       // CategoryOptions: CategoryOptions.data,
       YearOptions: YearOptions.data,
       MonthOptions: MonthOptions.data,
-      // TransmissionOptions: TransmissionOptions.data,
-      // GearTypeOptions: GearTypeOptions.data,
-      // GasTypeOptions: GasTypeOptions.data,
-      // EngineOptions: EngineOptions.data,
-      // PassengerOptions: PassengerOptions.data,
-      // ColorOptions: ColorOptions.data
+      TransmissionOptions: TransmissionOptions.data,
+      GearTypeOptions: GearTypeOptions.data,
+      GasTypeOptions: GasTypeOptions.data,
+      EngineDisplacementOptions: EngineDisplacementOptions.data,
+      PassengerOptions: PassengerOptions.data,
+      ColorOptions: ColorOptions.data,
+      EquipmentOptions: EquipmentOptions.data,
+      SafetyOptions: SafetyOptions.data
     }
   },
   data() {
@@ -201,24 +202,34 @@ export default {
       TransmissionOptions: [],
       GearTypeOptions: [],
       GasTypeOptions: [],
-      EngineOptions: [],
+      EngineDisplacementOptions: [],
       PassengerOptions: [],
       ColorOptions: [],      
-      EquipmentCheckAll1: false,
-      EquipmentCheckedOption1: [],
-      EquipmentIsIndeterminate1: false,
-      EquipmentOption1: EquipmentOption1,
-      EquipmentCheckAll2: false,
-      EquipmentCheckedOption2: [],
-      EquipmentIsIndeterminate2: false,
-      EquipmentOption2: EquipmentOption2,
+      EquipmentCheckAll: false,
+      EquipmentCheckedOption: [],
+      EquipmentIsIndeterminate: false,
+      EquipmentOptions: [],
+      SafetyCheckAll: false,
+      SafetyCheckedOption: [],
+      SafetyIsIndeterminate: false,
+      SafetyOptions: [],
       labelPosition: 'top',
       carForm: {
-        Brand: '',
-        Series: '',
-        Category: '',
-        Year: '',
-        Month: '',
+        Brand: 0,
+        Series: 0,
+        Category: 0,
+        Year: 0,
+        Month: 0,
+        Transmission: 0,
+        GearType: 0,
+        GasType: 0,
+        EngineDisplacement: 0,
+        Passenger: 0,
+        Color: 0,
+        Mileage: 0,
+        Price: 0,
+        Equipment: 0,
+        Safety: 0,
         Name: '',
         Cellphone: '',
         Address: ''
@@ -231,33 +242,64 @@ export default {
         console.log(JSON.stringify(newVal));
       },
       deep: true,
-      immediate: true,
+      // immediate: true,
     },
   },
   methods: {
     Create() {
-      
+      console.log("Echo")
     },
-    GetValue(type, value) {
+    Callback(type, value) {
       this.carForm[type] = value;
     },
-    handleCheckAllChange1(value) {
-      this.EquipmentCheckedOption1 = value ? EquipmentOption1 : [];
-      this.EquipmentIsIndeterminate1 = false;
+    EquipmentAllChange(value) {
+      this.EquipmentCheckedOption = value ? this.EquipmentOptions : [];
+      this.EquipmentIsIndeterminate = false;
+      if(value) {
+        for(var i = 0; i < this.EquipmentOptions.length; i++) {
+          this.carForm.Equipment += this.EquipmentOptions[i].value;
+        }
+      } else {
+        this.carForm.Equipment = 0;
+      }
+    },   
+    CheckedEquipmentChange(observer) {
+      let checkedCount = observer.length;
+      this.EquipmentCheckAll = checkedCount === this.EquipmentOptions.length;
+      this.EquipmentIsIndeterminate = checkedCount > 0 && checkedCount < this.EquipmentOptions.length;
+      if(checkedCount != 0) {
+        this.carForm.Equipment = 0;
+        for(var i = 0; i < observer.length; i++) {
+          this.carForm.Equipment += observer[i].value;
+        }
+      } else {
+        this.carForm.Equipment = 0;
+      }
     },
-    handleCheckAllChange2(value) {
-      this.EquipmentCheckedOption2 = value ? EquipmentOption2 : [];
-      this.EquipmentIsIndeterminate2 = false;
+    SafetyAllChange(value) {
+      console.log(value)
+      this.SafetyCheckedOption = value ? this.SafetyOptions : [];
+      this.SafetyIsIndeterminate = false;
+      if(value) {
+        for(var i = 0; i < this.SafetyOptions.length; i++) {
+          this.carForm.Safety += this.SafetyOptions[i].value;
+        }
+      } else {
+        this.carForm.Safety = 0;
+      }
     },
-    handleCheckedCitiesChange1(value) {
-      let checkedCount = value.length;
-      this.EquipmentCheckAll1 = checkedCount === this.EquipmentOption1.length;
-      this.EquipmentIsIndeterminate1 = checkedCount > 0 && checkedCount < this.EquipmentOption1.length;
-    },
-    handleCheckedCitiesChange2(value) {
-      let checkedCount = value.length;
-      this.EquipmentCheckAll2 = checkedCount === this.EquipmentOption2.length;
-      this.EquipmentIsIndeterminate2 = checkedCount > 0 && checkedCount < this.EquipmentOption2.length;
+    CheckedSafetyChange(observer) {
+      let checkedCount = observer.length;
+      this.SafetyCheckAll = checkedCount === this.SafetyOptions.length;
+      this.SafetyIsIndeterminate = checkedCount > 0 && checkedCount < this.SafetyOptions.length;
+      if(checkedCount != 0) {
+        this.carForm.Safety = 0;
+        for(var i = 0; i < observer.length; i++) {
+          this.carForm.Safety += observer[i].value;
+        }
+      } else {
+        this.carForm.Safety = 0;
+      }
     },
     handlePreview(file) {
       console.log("handlePreview")
